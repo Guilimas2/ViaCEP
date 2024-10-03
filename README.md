69370a5dbcc20543f57c184493bae931d93a6cf4
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-10-03 11:13:20 -0300

    inseri imagem no README e descrevi o que cada codigo faz

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Cadastro de endereço com consumo de API (ViaCEP)
+<h1 align="center">  Cadastro de endereço com consumo de API (ViaCEP) </h1>
+
+# Como funciona 
+esse site foi feito para você colocar seu CEP, quando você colocar seu CEP ira preencheer todos os campos como enedereço, bairro entre outros...
+
+
+# O que cada codigo faz 
+ hasOwnProperty: retorna um booleano indicando se o objeto possui a propriedade especificada como uma propriedade definida no próprio objeto em questão
+  
+ Await fetch:  O await faz com que o JavaScript espere até que a promessa seja resolvida (ou rejeitada).
+
+ ⁠json: é um formato de dados leve e de fácil leitura utilizado para troca de informações entre sistemas computacionais. 
+
+ ⁠async:  são uma maneira de escrever código assíncrono em JavaScript de uma forma mais lógica e legível. 
+
+# Sites consutados
+https://viacep.com.br/
+https://getbootstrap.com/
+
+# Pessoas que me ajudou no trabalho
+ [<img loading="lazy" src="" width=115><br><sub>Maria Yanagui </sub>](https://github.com/mariayanagui) |  [<img loading="lazy" src="yanagui.jpg" width=115><br><sub>Guilherme Serfim</sub>](https://github.com/Guilimas2) |  [<img loading="lazy" src="guieu.jpg" width=115><br><sub></sub>](h) 
--- main.js
+++ main.js
@@ -22,3 +22,29 @@ const preencherFormulario = (endereco) => {
     document.getElementById('cidade').valeu = endereco = cidade;
     document.getElementById('uf').valeu = endereco = uf;
 }
+// FUNÇÃO DE CONSUMO DE API VIA CEP
+// função ASSÍNCRONAS são úteis quando dependemos do resultado de alguma coisa para executar a função.
+const pesquisarcep = async() => {
+    // ASYNC essas funções podem realizar operações que demoram algum tempo, sem bloquear a execução do programa./  é uma forma de escrever funções que podem fazer várias coisas ao mesmo tempo, sem travar o programa
+    limparFormulario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+ 
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        // FETCH é um metodo do js que faz um pedido para via cep, e dar retorno.
+        const addres = await dados.json();
+        // JSON  é um formato leve para trocar dados, é usado principalmente para enviar e receber dados entre um cliente e um servidor.
+ 
+       
+        if(addres.hasOwnProperty('erro')){
+            alert('CEP não encontrado');
+        } else{
+            preencherFormulario(addres);
+        }
+    }else{
+        alert("CEP Incorreto!");
+    }
+}
+// ADICIONAR ESCUTADOR PARA EXECUTAR CONSUMO DE API DA ViaCep
+document.getElementById('cep').addEventListener('focusout', pesquisarcep);
+ 
